2b5eef66a34ecfe8b080873c4aeac0f6061908f7
Bastien Le Bret <blb@citygo.me>
2022-11-09 13:17:36 +0100

    Add jacoco (not working yet

--- src/main/java/org/jazzilla/core_build/JacocoConventionPlugin.kt
+++ src/main/java/org/jazzilla/core_build/JacocoConventionPlugin.kt
@@ -8,10 +8,9 @@ import org.gradle.api.tasks.testing.Test
 import org.gradle.configurationcache.extensions.capitalized
 import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.task
 import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
-import org.jazzilla.core_build.utils.apply
-import org.jetbrains.kotlin.gradle.utils.`is`
-import java.util.Locale
+import org.gradle.testing.jacoco.tasks.JacocoReport
 
 /**
  * Jacoco plugin must be applied in last position to get library our application plugin
@@ -28,31 +27,89 @@ class JacocoConventionPlugin : Plugin<Project> {
             tasks.withType(Test::class.java) {
                 with(this.extensions.getByType<JacocoTaskExtension>()) {
                     isIncludeNoLocationClasses = true
-                    exclude(listOf("jdk.internal.*"))
+                    excludes = listOf("jdk.internal.*")
                 }
             }
             
             val extension = extensions.findByType<BaseAppModuleExtension>()
                     ?: extensions.findByType<LibraryExtension>()
                     ?: throw IllegalStateException("Not extension configurable for Jacoco")
-    
-            val buildTypes = extension.buildTypes.map { it.name }
+            
+            extension.buildTypes {
+                getByName("debug") {
+                    enableUnitTestCoverage = true
+                    enableAndroidTestCoverage = false
+                }
+            }
+            
+            val buildTypes = extension.buildTypes.filter { it.enableUnitTestCoverage || it.enableAndroidTestCoverage }.map { it.name }
             val flavors = extension.productFlavors.map { it.name }.toMutableList()
             
             if(flavors.isEmpty()) flavors.add("")
             
+            val tasksList = mutableListOf<String>()
+            
             flavors.forEach { flavor ->
                 buildTypes.forEach { build ->
-                    val (sourceName, sourcePath) = if(flavor.isEmpty()) {
-                        build to build
+                    val sourceName = if(flavor.isEmpty()) {
+                        build
                     } else {
-                        "$flavor$build.capitalize()" to "$flavor/$build"
+                        "$flavor$build".capitalized()
                     }
+                    
                     
                     val taskName = "test${sourceName.capitalized()}UnitTest"
                     println("Task -> $taskName")
+                    tasksList.add(taskName)
+    
+                    task(name = "jacoco${taskName}Coverage", type = JacocoReport::class) {
+                        dependsOn(taskName)
+                        
+                        group = "Reporting"
+                        description = "Generate Jacoco coverage reports on the ${sourceName.capitalized()} build."
+    
+                        val kotlinTree = fileTree("$buildDir/tmp/kotlin-classes/$sourceName/")
+    
+                        additionalClassDirs(kotlinTree)
+                        executionData.from("$buildDir/jacoco/${taskName}.exec")
+    
+                        val coverageSourceDirs = files(
+                                "src/main/java",
+                                "src/$flavor/java",
+                                "src/$build/java"
+                        )
+                        
+                        sourceDirectories.from(coverageSourceDirs)
+                        additionalSourceDirs.from(coverageSourceDirs)
+                        
+                        reports {
+                            csv.required.set(true)
+                            xml.required.set(true)
+                            
+                            with(html) {
+                                required.set(true)
+                            }
+                        }
+                    }
                 }
             }
+    
+            /*task(name = "jacocoFullReport", type = JacocoReport::class) {
+                dependsOn(tasksList)
+        
+                group = "Reporting"
+                description = "Aggregate all Jacoco reports"
+                
+                reports {
+                    csv.required.set(false)
+                    xml.required.set(false)
+            
+                    with(html) {
+                        required.set(true)
+                        outputLocation.set(file("${buildDir}/coverage-report"))
+                    }
+                }
+            }*/
         }
     }
 }
